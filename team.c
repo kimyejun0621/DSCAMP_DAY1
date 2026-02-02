d74d57c2da018034975d68cf10ac88f393a0279e
kimyejun0621 <kimyejun0621@gmail.com>
2026-02-02 16:33:30 +0900

    asdf

--- team.c
+++ team.c
@@ -14,3 +14,20 @@ double get_average(Student *list,int count){
     avg = (double)sum / count ;
     return avg;
 }
+
+
+void check(Student *list, int count){
+    int sum = 0;
+    double avg = 0;
+
+    for(int i = 0; i < count; i++){
+        sum += list[i].score;
+    }
+    avg = (double)sum / count ;
+
+    for(int i = 0; i < count; i++){
+        if(avg > list[i].score){
+            printf("%s학생은 평균보다 낮습니다. 추가학습이 필요합니다.(%d)",list[i].name,list[i].score);
+        }
+    }
+}
